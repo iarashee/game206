7879464bdfcf02c3e6105c385b4d1861fc123b1a
Ibrahim Rasheed <irasheed@Ibrahims-MacBook-Pro-2.local>
2016-12-01 16:02:12 -0500

    [fixed increased speed]

--- mygame.py
+++ mygame.py
@@ -30,12 +30,12 @@ class Ball(Sprite):
         Sprite.__init__(self)
         self.image = image.load("ball.bmp")
         self.rect = self.image.get_rect()
-        self.rect.x = randint(10, display_h-10)
+        self.rect.x = randint(10, display_w-5)
         self.rect.y = 100
 
-    def move_down(self):
+    def move_down(self, dy):
         y = self.rect.y
-        y += 10
+        y += dy
         self.rect.y = y
 
     def reset(self):
@@ -51,68 +51,71 @@ sprites = RenderPlain(hoop, ball)
 time.set_timer(USEREVENT + 1, DELAY)
 
 f = font.Font(None, 25)
+score_f = font.Font(None, 50)
 
 mixer.music.load("blobtales.wav")
 mixer.music.play(-1)
 
 crashed = False
 game_off = False
-
-swoosh = 0
+score = 0
 while not crashed:
-    for e in event.get():
-        if e.type == KEYDOWN:
+    while not game_off:
+        for e in event.get():
+            if e.type == KEYDOWN:
+                
+                if e.key == K_LEFT:
+                    hoop.move(dx = -25)
+                
+                if e.key == K_RIGHT:
+                    hoop.move(dx = 25)
+                
+                if e.key == K_q:
+                    t = f.render("QUIT", False, (0,0,0)) # text not working
+                    screen.blit(t, (320, 0))
+                    game_off = True
             
-            if e.key == K_LEFT:
-                hoop.move(dx = -20)
-            
-            if e.key == K_RIGHT:
-                hoop.move(dx = 20)
-                if hoop.rect.x > (display_w-200):
-                    crashed = True
-            
-            if e.key == K_q:
-                t = f.render("QUIT", False, (0,0,0)) # text not working
-                screen.blit(t, (320, 0))
-                crashed = True
-
-        #if e.type == USEREVENT + 1:
-            #hoop.move()
-        
         if hoop.rect.colliderect(ball.rect):
             mixer.Sound("swoosh.wav").play()
-            swoosh += 1
+            score += 1
             ball.reset()
 
-        if swoosh > 5: ## Fix!! Needs to increase speed
-            DELAY -= 50
+        if score >= 0: ## Fix!! Needs to increase speed
+            ball.move_down(dy = 6)
+        if score >= 3: ## Fix!! Needs to increase speed
+            ball.move_down(dy = 6.25)
+        if score > 5: ## Fix!! Needs to increase speed
+            ball.move_down(dy = 6.35)    
+        if score > 7: ## Fix!! Needs to increase speed
+            ball.move_down(dy = 6.75)
+        if score > 9: ## Fix!! Needs to increase speed
+            ball.move_down(dy = 7)
 
         if ball.rect.y >= (display_h - 5):
-            t6 = f.render("Hit Bottom, Game Done", False, (0,0,0))
+            t6 = f.render("Hit Bottom, Game Done", True, (255,255,255))
             screen.blit(t6, (200, 350))
             game_off = True
 
-    screen = display.set_mode((display_w, display_h))
-    display.set_caption("Basketball!")
-    screen.blit(bg_image, (0,0))
+        if hoop.rect.x <= 0:
+            hoop.rect.x = 10
 
-    t1 = f.render("Baskets = " + str(swoosh), False, (0,0,0))
-    t2 = f.render("q = Quit", False, (0,0,0))
-    t3 = f.render("Directions: Catch the basketballs. Balls move down faster over time. You lose when you miss.", False, (0,0,0))
-    t4 = f.render(str(DELAY), False, (0,0,0))
-    t5 = f.render(str(ball.rect.y), False, (0,0,0))
-    
-    screen.blit(t1, (100, 100))
-    screen.blit(t2, (100, 150))
-    screen.blit(t3, (100, 200))
-    screen.blit(t4, (100, 250))
-    screen.blit(t5, (100, 300))
-    
-    
-    while not game_off:
-        ball.move_down()
+        if hoop.rect.x >= (display_w-342):
+            hoop.rect.x = (display_w-342)
 
-    #screen.fill(bgcolor)
-    sprites.update()
-    sprites.draw(screen)
-    display.update()
+        screen = display.set_mode((display_w, display_h))
+        display.set_caption("Basketball!")
+        screen.blit(bg_image, (0,0))
+
+        t1 = score_f.render("Score = " + str(score), False, (0,0,0))
+        t2 = f.render("q = Quit", False, (0,0,0))
+        t3 = f.render("Directions: Catch the basketballs. Balls move down faster over time. You lose when you miss.", False, (0,0,0))
+        
+        screen.blit(t1, (100, 100))
+        screen.blit(t2, (100, 150))
+        screen.blit(t3, (100, 200))
+
+
+        #screen.fill(bgcolor)
+        sprites.update()
+        sprites.draw(screen)
+        display.update()
